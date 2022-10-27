58b55db7253917ca7125333476d6162fd36ad71c
Pawel <roreth@wp.pl>
2022-10-27 19:36:14 +0200

    B-change-origin-to-netlify

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const io = require('socket.io')(3001, {
   cors: {
-    origin: 'http://127.0.0.1:5173',
+    origin: 'https://animated-cannoli-3bf6d7.netlify.app',
     methods: ['GET', 'POST'],
   },
 });
